184c17ab16710da470c8474bb41605ba9f130d21
UKnowNothingJonSn0w <67711329+UKnowNothingJonSn0w@users.noreply.github.com>
2022-09-25 18:29:26 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { RegionsComponent } from './regions/regions.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, MatGridListModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, RegionsComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
